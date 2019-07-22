df7fc4a8ea9e72f3bf4a9d58192a1a5644c0e693
Apple <apple@Apples-MacBook-Pro-36.local>
2019-07-22 14:11:40 -0500

    testing

---
+++ pat.playground/Contents.swift
@@ -0,0 +1,44 @@
+import Cocoa
+
+var str = "Hello, playground"//
+//string
+//integer
+//double
+//interpolation
+//variable
+//constant
+//keyword
+var address = "1313 Mockingbird Lane" //create or declare the variable address
+address = "254 Ocean Avenue" //changed the comments of addresss to a new address
+let name = "Hannah"
+var string = 5
+var empty : Double //data types are always written with a capital. when ypu are defining a variable
+var printme = "hello world"
+print(printme)
+var copyme = "bye earth"
+print(copyme)
+var firstname = "hannah"
+print (5+5)
+var test = 5
+var newTest = "5"
+print ( "hey this is math\(5+5)")
+print ( "hey my name is \(firstname)")
+//get out of bed
+//chase the dog of my clothes
+//look up the weather
+if temp > 80
+then wear shorts
+if temp < 80
+then wear pants
+var temp = 87
+
+if temp > 80 {
+    print ("wear shorts")
+}
+if it is 87
+//make a program that uses string interpolation to print a math problem and its solution
+if favoritefood = "popcorn" < "chipotle"
+print ("food")
+if starbucks" > "popcorn"
+print (eat)
+
